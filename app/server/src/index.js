a7924086e9cd688b9037274fe8e57cbae61fb1b2
victorx98 <victorx@gmail.com>
2019-02-26 14:10:00 -0800

    Add error handling for mysql connection lost

--- app/server/src/index.js
+++ app/server/src/index.js
@@ -8,25 +8,56 @@ import helmet from 'koa-helmet'
 import router from './routes'
 import { errorHandler } from './middleware'
 
-const app = new Koa()
+const app = new Koa();
+
+var fs = require('fs');
+var util = require('util');
+var log_file = fs.createWriteStream(__dirname + '/debug.log', {flags : 'w'});
+var log_stdout = process.stdout;
+
+console.log = function(d) { //
+  log_file.write(util.format(d) + '\n');
+  log_stdout.write(util.format(d) + '\n');
+};
+
+console.error = console.log;
 
 // set up mysql connection
 const mysql = require('mysql');
-const db = mysql.createConnection ({
+
+const db_config  = {
     host: 'localhost',
     user: 'resumake',
     password: 'resumake1357',
     database: 'resumes'
-});
+};
 
-// connect to database
-db.connect((err) => {
-    if (err) {
-        throw err;
+var connection;
+
+function handleDisconnect() {
+  connection = mysql.createConnection(db_config); // Recreate the connection, since
+                                                  // the old one cannot be reused.
+
+  connection.connect(function(err) {              // The server is either down
+    if(err) {                                     // or restarting (takes a while sometimes).
+      console.log('error when connecting to db:', err);
+      setTimeout(handleDisconnect, 2000); // We introduce a delay before attempting to reconnect,
+    }                                     // to avoid a hot loop, and to allow our node script to
+  });                                     // process asynchronous requests in the meantime.
+                                          // If you're also serving http, display a 503 error.
+  connection.on('error', function(err) {
+    console.log('db error', err);
+    if(err.code === 'PROTOCOL_CONNECTION_LOST') { // Connection to the MySQL server is usually
+      handleDisconnect();                         // lost due to either server restart, or a
+    } else {                                      // connnection idle timeout (the wait_timeout
+      throw err;                                  // server variable configures this)
     }
-    console.log('Connected to database');
-});
-global.db = db;
+  });
+}
+
+handleDisconnect();
+
+global.db = connection;
 
 if (app.env === 'development') {
   app.proxy = true
