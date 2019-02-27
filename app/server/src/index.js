810841166f4fe302797dacafb114773e00b3bfca
victorx98 <victorx@gmail.com>
2019-02-27 09:53:05 -0800

    Add better error logging when DB connection lost

--- app/server/src/index.js
+++ app/server/src/index.js
@@ -43,11 +43,13 @@ function handleDisconnect() {
       console.log('error when connecting to db:', err);
       setTimeout(handleDisconnect, 2000); // We introduce a delay before attempting to reconnect,
     }                                     // to avoid a hot loop, and to allow our node script to
+    console.log('DB connected');
   });                                     // process asynchronous requests in the meantime.
                                           // If you're also serving http, display a 503 error.
   connection.on('error', function(err) {
     console.log('db error', err);
     if(err.code === 'PROTOCOL_CONNECTION_LOST') { // Connection to the MySQL server is usually
+      console.log('DB Connection lost, reconnect...');
       handleDisconnect();                         // lost due to either server restart, or a
     } else {                                      // connnection idle timeout (the wait_timeout
       throw err;                                  // server variable configures this)
